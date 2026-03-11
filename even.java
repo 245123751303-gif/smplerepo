88ef149d144e286891d7b60590585fdd327c369f
245123751303-gif <245123751303@mvsrec.edu.in>
2026-03-11 13:26:20 +0530

    Set fixed number for even/odd check
    Removed user input for number and set it to a fixed value.

--- even.java
+++ even.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
-
 public class EvenOdd {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num;
-
-        System.out.print("Enter a number: ");
-        num = sc.nextInt();
+        int num = 8;
 
         if (num % 2 == 0)
             System.out.println("Even number");
